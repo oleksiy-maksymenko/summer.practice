093cba066cef1c53b6ddf57b73e63de59c80b29d
Oleksiy-me <Mo01022001>
2021-06-14 22:01:13 +0300

    <SP-64>: <Import skeleton of project to git>

---
+++ src/test/java/com/agile/vision/summer/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agile.vision.summer.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
